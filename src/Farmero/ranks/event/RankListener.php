040a282ad6675079e74d7b336ae584ff410c048c
Farmero1400 <145964153+Farmero1400@users.noreply.github.com>
2024-05-30 18:18:09 -0700

    Update RankListener.php

--- src/Farmero/ranks/event/RankListener.php
+++ src/Farmero/ranks/event/RankListener.php
@@ -7,7 +7,6 @@ namespace Farmero\ranks\event;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\player\Player;
 use pocketmine\player\chat\LegacyRawChatFormatter;
 use pocketmine\utils\TextFormat as TF;
 
@@ -18,19 +17,12 @@ class RankListener implements Listener {
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         Ranks::getInstance()->getRanksManager()->createPlayerProfile($player);
-        $this->updatePlayerDisplayName($player);
+        $this->plugin->updatePlayerDisplayName($player);
     }
 
     public function onPlayerChat(PlayerChatEvent $event) {
         $player = $event->getPlayer();
-        $rank = Ranks::getInstance()->getRanksManager()->getRank($player);
-        $rankDisplay = $rank ? Ranks::getInstance()->getRanksManager()->getRankDisplay($player) : "";
+        $rankDisplay = Ranks::getInstance()->getRanksManager()->getRankDisplay(Ranks::getInstance()->getRanksManager()->getRank($player));
         $event->setFormatter(new LegacyRawChatFormatter(TF::GREEN . "[" . $rankDisplay . "] " . $player->getName() . ": " . $event->getMessage()));
-    }
-
-    public function updatePlayerDisplayName(Player $player): void {
-        $rank = Ranks::getInstance()->getRanksManager()->getRank($player);
-        $rankDisplay = $rank ? Ranks::getInstance()->getRanksManager()->getRankDisplay($player) : "";
-        $player->setDisplayName(TF::GREEN . "[" . $rankDisplay . "] " . $player->getName());
     }
 }
