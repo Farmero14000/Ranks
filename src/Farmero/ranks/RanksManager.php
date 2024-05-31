abc2128e59dcb9664e55f0f17f94bb0c8a2f0602
Farmero1400 <145964153+Farmero1400@users.noreply.github.com>
2024-05-31 10:49:58 -0700

    Update RanksManager.php

--- src/Farmero/ranks/RanksManager.php
+++ src/Farmero/ranks/RanksManager.php
@@ -176,6 +176,7 @@ class RanksManager {
             unset($this->tempRanksData[$player->getName()]);
             $this->updatePlayerDisplayName($player);
         }
+    }
 
     public function getTempRankData(Player $player): ?array {
         return $this->tempRanksData[$player->getName()] ?? null;
