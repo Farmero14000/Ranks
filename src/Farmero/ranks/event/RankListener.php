da2ca02d1581bf27b5eaf6ba31456ebeb80a6a76
Farmero14000 <145265002+Farmero14000@users.noreply.github.com>
2024-06-01 17:18:54 -0700

    Update RankListener.php

--- src/Farmero/ranks/event/RankListener.php
+++ src/Farmero/ranks/event/RankListener.php
@@ -8,7 +8,6 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\player\chat\LegacyRawChatFormatter;
-use pocketmine\utils\TextFormat as TF;
 
 use Farmero\ranks\Ranks;
 
@@ -23,9 +22,8 @@ class RankListener implements Listener {
 
     public function onPlayerChat(PlayerChatEvent $event) {
         $player = $event->getPlayer();
-        $ranksManager = Ranks::getInstance()->getRanksManager();
-        $rank = $ranksManager->getRank($player);
-        $rankDisplay = $ranksManager->getRankDisplay($rank);
-        $event->setFormatter(new LegacyRawChatFormatter(TF::GREEN . "[" . $rankDisplay . "] " . $player->getName() . ": " . $event->getMessage()));
-    }
+        $rank = Ranks::getInstance()->getRanksManager()->getRank($player);
+        $rankChatFormat = $rank ? Ranks::getInstance()->getRanksManager()->getChatFormat($rank) : "{playerName}: {message}";
+        $formattedMessage = str_replace(["{playerName}", "{message}"], [$player->getName(), $event->getMessage()], $rankChatFormat);
+        $event->setFormatter(new LegacyRawChatFormatter($formattedMessage));
 }
